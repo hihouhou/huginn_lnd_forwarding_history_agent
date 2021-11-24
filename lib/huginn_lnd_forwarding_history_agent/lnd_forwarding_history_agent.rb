eefa8936a82782a26f8d0e69e7c1025edf7c82ee
Nicolas Germain <ngermain@hihouhou.com>
2021-11-24 21:43:51 +0100

    resolving nodename feature added

--- lib/huginn_lnd_forwarding_history_agent/lnd_forwarding_history_agent.rb
+++ lib/huginn_lnd_forwarding_history_agent/lnd_forwarding_history_agent.rb
@@ -13,7 +13,7 @@ module Agents
 
       `url` for the lnd url like https://127.0.0.1:8080
 
-      `num_max_events` for the max result wanted
+      `resolve_nodename` for the max result wanted
 
       `start_time` is used for fetching since X days ago
 
@@ -41,7 +41,7 @@ module Agents
     def default_options
       {
         'url' => '',
-#        'num_max_events' => '10',
+        'resolve_nodename' => 'false',
         'start_time' => '1',
         'debug' => 'false',
         'changes_only' => 'true',
@@ -51,7 +51,7 @@ module Agents
     end
 
     form_configurable :url, type: :string
-#    form_configurable :num_max_events, type: :string
+    form_configurable :resolve_nodename, type: :boolean
     form_configurable :start_time, type: :string
     form_configurable :changes_only, type: :boolean
     form_configurable :macaroon, type: :string
@@ -62,10 +62,6 @@ module Agents
       unless options['url'].present?
         errors.add(:base, "url is a required field")
       end
-#
-#      unless options['num_max_events'].present?
-#        errors.add(:base, "num_max_events is a required field")
-#      end
 
       unless options['start_time'].present?
         errors.add(:base, "start_time is a required field")
@@ -73,6 +69,10 @@ module Agents
 
       if options.has_key?('changes_only') && boolify(options['changes_only']).nil?
         errors.add(:base, "if provided, changes_only must be true or false")
+      end
+
+      if options.has_key?('resolve_nodename') && boolify(options['resolve_nodename']).nil?
+        errors.add(:base, "if provided, resolve_nodename must be true or false")
       end
 
       if options.has_key?('debug') && boolify(options['debug']).nil?
@@ -103,7 +103,6 @@ module Agents
       uri = URI.parse("#{interpolated['url']}/v1/switch")
       request = Net::HTTP::Post.new(uri)
       request["Grpc-Metadata-Macaroon"] = "#{interpolated['macaroon']}"
-#      request.body = "{ \"num_max_events\": #{interpolated['num_max_events']}}"
       request.body = "{ \"start_time\": \"#{timestamp_wanted}\"}"
       
       req_options = {
@@ -127,6 +126,11 @@ module Agents
       if interpolated['debug'] == 'true'
         log "payload"
         log payload
+      end
+
+      if interpolated['resolve_nodename'] == 'true'
+        log "fetching channels"
+        channels = get_channels
       end
       if interpolated['changes_only'] == 'true'
         if payload.to_s != memory['last_status']
@@ -156,6 +160,18 @@ module Agents
                   log "found is #{found}! so event created"
                   log event
                 end
+                if interpolated['resolve_nodename'] == 'true'
+                  channels['channels'].each do |channel|
+                    if channel['chan_id'] == event['chan_id_in']
+                      log "channel id found"
+                      event['chan_id_in'] = get_alias(channel['remote_pubkey'])
+                    end
+                    if channel['chan_id'] == event['chan_id_out']
+                      log "channel id found"
+                      event['chan_id_out'] = get_alias(channel['remote_pubkey'])
+                    end
+                  end
+                end
                 create_event payload: event
               end
             end
@@ -168,6 +184,77 @@ module Agents
           memory['last_status'] = payload.to_s
         end
       end
+    end
+
+    def get_channels
+      uri = URI.parse("#{interpolated['url']}/v1/channels")
+      request = Net::HTTP::Get.new(uri)
+      request["Grpc-Metadata-Macaroon"] = "#{interpolated['macaroon']}"
+      request.body = "{ \"active_only\": \"true\"}"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+        verify_mode: OpenSSL::SSL::VERIFY_NONE,
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      log "request  status : #{response.code}"
+
+      if interpolated['debug'] == 'true'
+        log "response.body"
+        log response.body
+      end
+
+      payload = JSON.parse(response.body)
+
+      if interpolated['debug'] == 'true'
+        log "payload"
+        log payload
+      end
+      return payload
+    end
+
+    def get_alias(nodeid)
+      uri = URI.parse("https://1ml.com/node/#{nodeid}/json")
+      request = Net::HTTP::Get.new(uri)
+      request["Authority"] = "1ml.com"
+      request["Cache-Control"] = "max-age=0"
+      request["Upgrade-Insecure-Requests"] = "1"
+      request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+      request["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      request["Sec-Gpc"] = "1"
+      request["Sec-Fetch-Site"] = "same-origin"
+      request["Sec-Fetch-Mode"] = "navigate"
+      request["Sec-Fetch-User"] = "?1"
+      request["Sec-Fetch-Dest"] = "document"
+      request["Referer"] = "https://1ml.com/node/#{nodeid}"
+      request["Accept-Language"] = "fr,en-US;q=0.9,en;q=0.8"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      log "request  status : #{response.code}"
+
+      if interpolated['debug'] == 'true'
+        log "response.body"
+        log response.body
+      end
+
+      payload = JSON.parse(response.body)
+
+      if interpolated['debug'] == 'true'
+        log "payload"
+        log payload
+      end
+      return payload['alias']
     end
   end
 end
